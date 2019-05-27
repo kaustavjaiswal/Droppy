35d348409e90ea7caf078d18c56a0d7487958bf9
kkaushikjaiswal <kaustavkaushik.jaiswal@amadeus.com>
2019-05-27 15:41:10 +0530

    Looking Good

--- library/src/main/java/com/shehabic/droppy/DroppyMenuPopup.kt
+++ library/src/main/java/com/shehabic/droppy/DroppyMenuPopup.kt
@@ -225,7 +225,7 @@ open class DroppyMenuPopup constructor(
         val anchorPosition = anchorCoordinates
         var finalX = anchorPosition.x + xOffset
         val anchorHeight = anchor.height
-        var finalY = anchorPosition.y
+        var finalY = anchorPosition.y + yOffset
         val screen = screenSize
         val rightMargin = screen.x - (finalX + mPopupView.measuredWidth)
         if (rightMargin < 0) {
